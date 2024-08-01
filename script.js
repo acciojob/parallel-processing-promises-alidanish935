f05d603781d8d2b36e14a2279c30f97e399e95cc
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-08-01 15:49:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+btn.addEventListener('click',()=>{
+	const dawnloadPromisses = images.map(dawnloadImage)
+	Promise.all(dawnloadPromisses)
+	.then(displayImage)
+	.catch(error=>console.log(error))
+})
+
+function dawnloadImage(image){
+	return new Promise((resolve,reject)=>{
+		const img = new Image();
+		img.onload = ()=> resolve(img);
+		img.onerror = ()=> reject(new Error(`Failed to load image's URL: ${image.url}`));
+		img.src = image.url;
+	})
+}
+function displayImage(images){
+	images.forEach(img=> output.appendChild(img));
+}
